7792dc99c3b1559e624a1e0d5ceeb63e1dd90574
Ido <idopaliaso@gmail.com>
2024-05-26 17:36:32 +0300

    commit

--- main.tf
+++ main.tf
@@ -11,33 +11,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "iam_assumable_role" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-
-  trusted_role_arns = [
-    "arn:aws:iam::905418055021:user/admin"
-  ]
-
-  create_role = true
-
-  role_name         = "ecs-task-execution-role"
-  role_requires_mfa = false
-
-  custom_role_policy_arns = [
-    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  ]
-  number_of_custom_role_policy_arns = 2
+# iam_role module
+module "iam_role" {
+  source = "./modules/iam_role"
 }
-
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs-task-execution-role"
-}
-
-# # iam_role module
-# module "iam_role" {
-#   source = "./modules/iam_role"
-# }
 
 # ecs module
 module "ecs" {
@@ -53,12 +30,12 @@ module "ecs" {
   task_definition_container_port = var.task_definition_container_port
   launch_type                    = var.launch_type
   desired_count                  = var.desired_count
-  execution_role_arn             = data.aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn             = module.iam_role.execution_role_arn.execution_role_arn
   api_gateway_stage              = var.api_gateway_stage
   public_subnet_1_id             = data.aws_subnet.public_subnet.id
   aws_lb_target_group_arn        = data.aws_lb_target_group.target_group.id
   aws_security_group_id          = data.aws_security_group.security_group.id
   log_group_name                 = var.log_group_name
 
-  # depends_on = [module.iam_role]
+  depends_on = [module.iam_role]
 }
